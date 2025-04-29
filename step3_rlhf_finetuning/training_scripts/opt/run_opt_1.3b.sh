28235cfe76f36c7909c90ffd6f09f38e11232c77
Siyuan Xu <786912649@qq.com>
2025-04-29 08:31:00 -0400

    add

--- step3_rlhf_finetuning/training_scripts/opt/run_opt_1.3b.sh
+++ step3_rlhf_finetuning/training_scripts/opt/run_opt_1.3b.sh
@@ -13,9 +13,9 @@ export TOKENIZERS_PARALLELISM=False
 
 DEV=1
 PORT=1236
-DATA_PATH="/gpuhome/hbz5148/workspace/siyuan/ReMax/dataset/Dahoas/full-hh-rlhf"
-ACTOR_MODEL_PATH=facebook/opt-1.3b
-REWARD_MODEL_PATH=~/workspace/siyuan/ReMax/step2_reward_model_finetuning/output/opt-1.3b/Dahoas/full-hh-rlhf
+DATA_PATH="/gpuhome/hbz5148/workspace/siyuan/retry/dataset/Dahoas/full-hh-rlhf"
+ACTOR_MODEL_PATH=~/workspace/siyuan/retry/step1_supervised_finetuning/output/opt-1.3b/Dahoas/full-hh-rlhf
+REWARD_MODEL_PATH=~/workspace/siyuan/retry/step2_reward_model_finetuning/output/opt-1.3b/Dahoas/full-hh-rlhf
 ACTOR_ZERO_STAGE=2
 REWARD_ZERO_STAGE=3
 REFERENCE_ZERO_STAGE=3
