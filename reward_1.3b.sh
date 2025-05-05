16312a5b8303040bddf29e1f9d748cad91dea82f
Siyuan Xu <786912649@qq.com>
2025-05-05 12:38:22 -0400

    add

--- reward_1.3b.sh
+++ reward_1.3b.sh
@@ -6,7 +6,7 @@ MODEL_NAME="PKU-Alignment/beaver-7b-v3.0-reward"
 #MODEL_NAME=~/workspace/siyuan/ReMax/step2_reward_model_finetuning/output/opt-1.3b/full-hh-rlhf
 
 CUDA_VISIBLE_DEVICES=2 python reward_eval.py \
-    --data_path "/gpuhome/hbz5148/workspace/siyuan/retry/dataset/tldr/train.json" \
-    --new_data_path "/gpuhome/hbz5148/workspace/siyuan/retry/dataset/tldr/test.json" \
+    --data_path "/gpuhome/hbz5148/workspace/siyuan/retry/dataset/Dahoas/full-hh-rlhf/train.json" \
+    --new_data_path "/gpuhome/hbz5148/workspace/siyuan/retry/dataset/Dahoas/full-hh-rlhf/test.json" \
     --model_name_or_path_reward $MODEL_NAME 
     
